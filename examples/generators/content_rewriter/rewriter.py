6d18b221034ad9e190f1460670dc6a85addf8152
Shuai Lin <shuailin97@gmail.com>
2020-06-27 23:19:32 +0800

    Update rewriter.py

--- examples/generators/content_rewriter/rewriter.py
+++ examples/generators/content_rewriter/rewriter.py
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2019 The Forte Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,23 +18,56 @@ The re-writer processor
 from forte.data.data_pack import DataPack
 from forte.processors.base import PackProcessor
 from ft.onto.base_ontology import Utterance, UtteranceContext
-
+import subprocess
+import os
 
 class ContentRewriter(PackProcessor):
     def _process(self, input_pack: DataPack):
         context = input_pack.get_single(UtteranceContext)
         utterance = input_pack.get_single(Utterance)
 
-        print('Now you can use the utterance and context to do prediction')
+        # Step 1: prepare the input data file
+        type = []
+        val = []
+        asso = []
+        # print(context.text)
 
-        print('The input context is:')
-        print(context.text)
+        for triple in context.text.split():
+            # print(triple)
+            for idx, i in enumerate(triple.split('|')):
+                if not idx:
+                    val.append(i)
+                elif idx == 1:
+                    type.append(i)
+                else:
+                    asso.append(i)
+        data_dir = '/data3/linshuai/manip_old/examples/text_content_manipulation/e2ev14_demo/test'
+        with open('{}/x_type.test.txt'.format(data_dir), 'w') as f_type, \
+                open('{}/x_value.test.txt'.format(data_dir), 'w') as f_val, \
+                open('{}/x_associated.test.txt'.format(data_dir), 'w') as f_asso, \
+                open('{}/y_ref.test.txt'.format(data_dir), 'w') as f_ref:
+            f_type.write(' '.join(type))
+            f_val.write(' '.join(val))
+            f_asso.write(' '.join(asso))
+            f_ref.write(utterance.text)
 
-        print('The utterance is:')
-        print(utterance.text)
+        # Step 2: Restore the generation model and test
+        os.system('./run.sh')
 
-        print('You can generate a new utterance like this.')
-        response = "This is a sample response."
+        # step 3: process the output file
+        output_dir = '/data3/linshuai/manip_old/examples/text_content_manipulation/e2ev14_output'
+        with open('{}/demo/ckpt/hypos.step921.test.txt'.format(output_dir), 'r') as f_hypo:
+            hypo = f_hypo.read()
+
+        response = hypo.replace("_", " ")
+        # print('Now you can use the utterance and context to do prediction')
+
+        print('The input context/table is:{}'.format(context.text))
+
+
+        print('The utterance is:{}'.format(utterance.text.replace('_', ' ')))
+
+        print('Generation sentence:{}'.format(response))
 
         # First add the text here.
         input_pack.set_text(input_pack.text + '\n' + response)
